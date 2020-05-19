d9ecdc9eb14dc5020127b0b1ebfc6db3cb0e468b
Johannes B <johannes.bock@live.de>
2020-05-19 15:01:29 +0200

    Projektdateien hinzufügen.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Asteroids_T22_B
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Asteroid> Asteroiden = new List<Asteroid>();
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(17);
+            //timer.Start();
+            timer.Tick += Animate;            
+        }
+
+        private void Animate(object sender, EventArgs e)
+        {
+            Zeichenfläche.Children.Clear();
+            foreach (Asteroid item in Asteroiden)
+            {
+                item.Draw(Zeichenfläche);
+                item.Move(timer.Interval, Zeichenfläche);
+            }
+        }
+
+        private void Btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+            for (int i = 0; i < 20; i++)
+            {
+                Asteroiden.Add(new Asteroid(Zeichenfläche));
+            }
+            Btn_Start.IsEnabled = false;
+        }
+    }
+}
---
+++ Spielobjekte.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace Asteroids_T22_B
+{
+    abstract class Spielobjekte
+    {
+        public double x { get; private set; }
+        public double y { get; private set; }
+
+        protected double vx, vy;
+
+        public Spielobjekte(double x0, double y, double vx, double vy)
+        {
+            x = x0;
+            this.y = y;
+            this.vy = vy;
+            this.vx = vx;
+        }
+
+        abstract public void Draw(Canvas Zeichenfläche);
+
+        public void Move(TimeSpan t, Canvas Zeichenfläche)
+        {
+            x += vx * t.TotalSeconds;
+            y += vy * t.TotalSeconds;
+
+            if (x < 0) x = Zeichenfläche.ActualWidth;
+            if (x > Zeichenfläche.ActualWidth) x = 0;
+            if (y < 0) y = Zeichenfläche.ActualHeight;
+            if (y > Zeichenfläche.ActualHeight) y = 0;
+        }
+
+    }
+
+    class Asteroid : Spielobjekte
+    {
+        static Random rnd = new Random();
+
+        public Asteroid(Canvas Zeichenfläche)
+            : base (rnd.NextDouble()*Zeichenfläche.ActualWidth,
+                    rnd.NextDouble()*Zeichenfläche.ActualHeight,
+                    rnd.NextDouble()*200-100,
+                    rnd.NextDouble()*200-100)
+        {
+
+        }
+
+        public override void Draw(Canvas Zeichenfläche)
+        {
+            Polygon umriss = new Polygon();
+            for (int i = 0; i < 5; i++)
+            {
+                double radius = 10;
+                double winkel = 2 * Math.PI / 5 * i;
+                umriss.Points.Add(new Point(radius*Math.Cos(winkel),
+                                            radius*Math.Sin(winkel)));
+            }
+
+
+            umriss.Fill = Brushes.Gray;
+            Zeichenfläche.Children.Add(umriss);
+            Canvas.SetLeft(umriss, x);
+            Canvas.SetTop(umriss, y);
+        }
+    }
+
+    //class Raumschiff : Spielobjekte
+    //{
+
+    //}
+
+    //class Torpedo : Spielobjekte
+    //{
+
+    //}
+}
